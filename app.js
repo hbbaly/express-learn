7682660160e394093c6bcb478fb0d981a797d12c
hbbaly <1422699902@qq.com>
2019-05-30 16:23:04 +0800

    ADD: 添加模版引擎

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const app = express();
-const logger = require('morgan')
-app.use(logger('dev'))
-// app.use(express.static('public'));
-app.use('/static', express.static('public'));
-// 示例中间件函数
-const middleware = (req, res, next) => {
-  // ... 进行一些操作
-  console.log('in')
-  next(); // 调用 next() ，Express 将调用处理链中下一个中间件函数。
-  console.log('out')
-};
+const path = require('path')
+// 设置包含模板的文件夹（'views'）
+app.set('views', path.join(__dirname, 'views'));
+// 设置视图引擎，比如'jade'
+app.set('view engine', 'pug');
 
-// 用 use() 为所有的路由和动词添加该函数
-// app.use(middleware);
+app.get('/', (req, res) => {
+  res.render('index', { title: '关于狗狗', message: '狗狗很牛！' });
+});
 
-// 用 use() 为一个特定的路由添加该函数
-// app.use('/someroute', middleware);
-
-// 为一个特定的 HTTP 动词和路由添加该函数
-app.get('/', middleware);
-
-const wiki = require('./routes/wiki.js');
-app.use('/wiki', wiki);
 app.listen(3000, (req, res, next) => {
   console.log('示例应用正在监听 3000 端口!')
 })
